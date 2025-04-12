155c6cc659a9fee0dad3de099a28d5f473010d77
HudsonCollier <hudsondcollier@gmail.com>
2025-04-11 21:22:04 -0600

    Added test background image and setup homescreen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,19 @@
 import { Text, View, StyleSheet } from 'react-native';
- import { Link } from 'expo-router'; 
+
+import Button from '@/components/Button';
+import ImageViewer from '@/components/imageViewer';
+const placeholder = require('@/assets/images/background.png')
 
 export default function Index() {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Welcome to Euro Timetables</Text>
-      <Link href="/about" style={styles.button}>
-        About us
-      </Link>
+      <View style={styles.imageContainer}>
+        <ImageViewer imgSource = {placeholder}/>
+      </View>
+      <View style={styles.footerContainer}>
+        <Button label="Find your station" />
+      </View>
     </View>
   );
 }
@@ -17,14 +23,15 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#25292e',
     alignItems: 'center',
-    justifyContent: 'center',
   },
   text: {
     color: '#fff',
   },
-  button: {
-    fontSize: 20,
-    textDecorationLine: 'underline',
-    color: '#fff',
+  imageContainer: {
+    flex: 1,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
   },
 });
